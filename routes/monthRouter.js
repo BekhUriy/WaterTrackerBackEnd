a9f1ff5a3af53acd9bb3c59014092f213630e5d4
zorianach <zoryanamirchuk@gmail.com>
2024-04-01 19:58:36 +0300

    Functions fixes

--- routes/monthRouter.js
+++ routes/monthRouter.js
@@ -1,13 +1,12 @@
-// waterRouter.get("/month", getWaterByMonth);
-
 import express from "express";
-// import validateBody from "../helpers/validateBody";
 import monthController from "../controllers/month-controller.js"
 import validateQuery from "../helpers/validateQuery.js";
-import { dateSchema } from "../schemas/dateSchema.js";
+
+import { monthAddSchema } from "../schemas/dateSchema.js";
+import auth from "../middlewares/auth.js";
 
 const monthRouter = express.Router();
 
-monthRouter.get("/", validateQuery(dateSchema), monthController.getMonthStatictics);
+monthRouter.get("/", auth, validateQuery(monthAddSchema), monthController.getMonthStatictics);
 
 export default monthRouter;
--- schemas/dateSchema.js
+++ schemas/dateSchema.js
@@ -1,5 +1,29 @@
 import Joi from "joi";
 
-export const dateSchema = Joi.date()
+export const todayDateSchema = Joi.date()
 .max('now')
 .required() ;
+
+export const monthDateSchema = Joi.object({
+    date: Joi.string()
+      .regex(/^\d{4}-\d{2}-\d{2}$/)
+      .required()
+      .messages({
+        "string.pattern.base": '"date" must be in the format "yyyy-mm-dd"',
+      }),
+  });
+
+// export const monthAddSchema = Joi.object({
+//     startDate: Joi.string()
+//       .regex(/^\d{4}-\d{2}-\d{2}$/)
+//       .required()
+//       .messages({
+//         "string.pattern.base": '"date" must be in the format "yyyy-mm-dd"',
+//       }),
+//     endDate: Joi.string()
+//       .regex(/^\d{4}-\d{2}-\d{2}$/)
+//       .required()
+//       .messages({
+//         "string.pattern.base": '"date" must be in the format "yyyy-mm-dd"',
+//       }),
+//   });
