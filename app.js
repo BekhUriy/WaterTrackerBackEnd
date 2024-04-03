dbc5fbeb0d968a3001941187a3e189c9a1ceb656
zorianach <zoryanamirchuk@gmail.com>
2024-04-03 19:55:51 +0300

    Swagger config

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
+import { readFile } from "fs/promises";
 import router from "./routes/index.js";
 import "./db.js";
+import swaggerUi from "swagger-ui-express";
+
 
 const app = express();
+
+const swaggerDocument = JSON.parse(
+  await readFile(new URL("./swagger.json", import.meta.url))
+);
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
